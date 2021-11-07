6e82176b67c25cae09489a5a2329cc16e1218aff
dillon <dhirth.it@gmail.com>
2021-11-07 02:44:21 -0600

    adding another test

--- Test/test_decks.py
+++ Test/test_decks.py
@@ -3,3 +3,7 @@ from ..decks import *
 
 def test_value_between_two_and_ten():
     assert Card(10, "heart").value in range(1, 11)
+
+def test_generate_deck_count():
+    d = MainDeck()
+    assert len(d.generated_deck) == 36
