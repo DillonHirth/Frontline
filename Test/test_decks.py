601a6231e8defab216aa5fce51f71ee057a3f715
dillon <dhirth.it@gmail.com>
2021-11-11 01:03:19 -0600

    updated tests, setup SupportDeck class
    I think i can cleanly combine the two deck classes using @classmethod constructors. plan on reading more on this as that would be cool.

--- Test/test_decks.py
+++ Test/test_decks.py
@@ -1,17 +1,36 @@
 from ..decks import *
 
-d = MainDeck()
+main_deck_test = MainDeck()
+support_deck_test = SupportDeck()
+
+suit_list = ['Spade', 'Diamond', 'Club', 'Heart']
+value_list = ['Jack', 'Queen', 'King', 'Ace', 'Joker']
+value_range = range(2, 11)
 
 
-def test_value_between_two_and_ten():
-    for item in d.generated_deck:
+def test_maindeck_value_and_suit():
+    for item in main_deck_test.generated_deck:
         assert item.value in range(2, 11) and item.suit in ['Spade', 'Diamond', 'Club', 'Heart']
 
 
-def test_decks_card_count():
-    assert d.card_count() == 36
+def test_maindeck_decks_card_count():
+    assert main_deck_test.card_count() == 36
 
 
-def test_decks_draw():
-    card = d.draw()
-    assert card.value in range(2, 11) and card.suit in ['Spade', 'Diamond', 'Club', 'Heart'] and d.card_count() == 35
+def test_maindeck_decks_draw():
+    card = main_deck_test.draw()
+    assert card.value in value_range and card.suit in suit_list and main_deck_test.card_count() == 35
+
+
+def test_supportdeck_value_and_suit():
+    for item in support_deck_test.generated_deck:
+        assert item.value in value_list and item.suit in suit_list
+
+
+def test_supportdeck_decks_card_count():
+    assert support_deck_test.card_count() == 20
+
+
+def test_supportdeck_decks_draw():
+    card = support_deck_test.draw()
+    assert card.value in value_list and card.suit in suit_list and support_deck_test.card_count() == 19
--- decks.py
+++ decks.py
@@ -2,8 +2,12 @@ import random
 
 
 class Card:
-    """
-    basic card object, has value and suit
+    """Class docstrings should contain the following information:
+
+    A brief summary of its purpose and behavior
+    Any public methods, along with a brief description
+    Any class properties (attributes)
+    Anything related to the interface for subclassers, if the class is intended to be subclassed
     """
 
     def __init__(self, value, suit):
@@ -15,9 +19,10 @@ class Card:
 
 
 class MainDeck:
-    """main army deck, generates a 36 count deck of number cards
+    """main army deck, generates a 36 count deck of numerical cards
 
     Methods:
+    generate_main_deck
     card_count
     draw
     shuffle
@@ -30,6 +35,8 @@ class MainDeck:
         return "generated_deck: {}".format(self.generated_deck)
 
     def generate_main_deck(self):
+        """generates a list of 36 CARD objects, with a numerical value for each suit"""
+
         suit_list = ['Spade', 'Diamond', 'Club', 'Heart']
         value_list = [2, 3, 4, 5, 6, 7, 8, 9, 10]
         deck_list = []
@@ -39,22 +46,61 @@ class MainDeck:
         return deck_list
 
     def card_count(self):
+        """returns the length of the generated deck"""
+
         return len(self.generated_deck)
 
     def draw(self):
+        """pops the first card off of the deck and returns it"""
+
         return self.generated_deck.pop(0)
 
     def shuffle(self):
+        """uses random.shuffle() to shuffle the list and returns a reference to the MainDeck object"""
+
         random.shuffle(self.generated_deck)
         return self
 
 
 class SupportDeck(Card):
+    """main army deck, generates a 36 count deck of numerical cards
+
+    Methods:
+    generate_main_deck
+    card_count
+    draw
+    shuffle
     """
-    """
-    suit = ['Spade', 'Diamond', 'Club', 'Heart']
-    value_list = ['Jack', 'Queen', 'King', 'Ace', 'Joker']
-    ## methods
-    # count()
-    # draw()
-    # shuffle()
+
+    def __init__(self):
+        self.generated_deck = self.generate_main_deck()
+
+    def __repr__(self):
+        return "generated_deck: {}".format(self.generated_deck)
+
+    def generate_main_deck(self):
+        """generates a list of 36 CARD objects, with a numerical value for each suit"""
+
+        suit_list = ['Spade', 'Diamond', 'Club', 'Heart']
+        value_list = ['Jack', 'Queen', 'King', 'Ace', 'Joker']
+        deck_list = []
+        for suit in suit_list:
+            for value in value_list:
+                deck_list.append(Card(value, suit))
+        return deck_list
+
+    def card_count(self):
+        """returns the length of the generated deck"""
+
+        return len(self.generated_deck)
+
+    def draw(self):
+        """pops the first card off of the deck and returns it"""
+
+        return self.generated_deck.pop(0)
+
+    def shuffle(self):
+        """uses random.shuffle() to shuffle the list and returns a reference to the MainDeck object"""
+
+        random.shuffle(self.generated_deck)
+        return self
