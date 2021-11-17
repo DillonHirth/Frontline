2eadc658bcbdc4f49d8839f7a411dc07fc38e827
dillon <dhirth.it@gmail.com>
2021-11-16 23:10:38 -0600

    added method text, and started the hand/player modules

--- hand.py
+++ hand.py
@@ -1,11 +1,18 @@
 class Hand:
-    """Hand class, owned by player
+    """Hand class, owned by player. Deck needs to be instantiated first
 
     Methods:
         sort
         play_card
     """
+
+    def __init__(self, draw_count):
+        self.draw_count = draw_count
+
+    def __repr__(self):
+        return None
+
+
     ##CardCount()
     ##PointCount()
     ##PlayCard()
-    pass
--- player.py
+++ player.py
@@ -1,7 +1,21 @@
+from decks import Deck as Deck
+
+
 class Player:
     """Player class that should own a 2 decks, a hand of cards, and half the game-board
 
     Methods:
         draw_hand
     """
-    pass
+
+    def __init__(self, player_name):
+        suit_list = ['Spade', 'Diamond', 'Club', 'Heart']
+        numerical_value_list = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+        face_value_list = ['Jack', 'Queen', 'King', 'Ace', 'Joker']
+
+        self.player_name = player_name
+        self.main_deck = Deck(numerical_value_list, suit_list)
+        self.support_deck = Deck(face_value_list, suit_list)
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}s, Skill: {self.ability}"
